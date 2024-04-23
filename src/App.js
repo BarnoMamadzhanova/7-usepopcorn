dd72fe965c88eb6f16c660510b715a32143db4c1
BarnoMamadzhanova <b@mamadzhanova.com>
2024-04-23 22:22:16 +0200

    passing elements as props - alternative to children

--- src/App.js
+++ src/App.js
@@ -61,13 +61,22 @@ export default function App() {
         <NumResalts movies={movies} />
       </Navbar>
       <Main>
-        <Box>
+        <Box element={<MovieList movies={movies} />} />
+        <Box
+          element={
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMovieList watched={watched} />
+            </>
+          }
+        />
+        {/* <Box>
           <MovieList movies={movies} />
         </Box>
         <Box>
           <WatchedSummary watched={watched} />
           <WatchedMovieList watched={watched} />
-        </Box>
+        </Box> */}
       </Main>
     </>
   );
@@ -117,7 +126,7 @@ function Main({ children }) {
   return <main className="main">{children}</main>;
 }
 
-function Box({ children }) {
+function Box({ element }) {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
@@ -126,7 +135,7 @@ function Box({ children }) {
         {isOpen ? "–" : "+"}
       </button>
 
-      {isOpen && children}
+      {isOpen && element}
     </div>
   );
 }
